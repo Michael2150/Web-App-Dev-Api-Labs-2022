183a255e42726f1c7a470d73fab167c666bc272a
Michael2150 <michaelgerb2150@gmail.com>
2022-11-22 22:02:58 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './api/movies';
-import genresRouter from './api/genres';
 import './db';
 import './seedData';
+import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at http://localhost:${port}/api/movies`);
